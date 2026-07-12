db84cac25e68fe70039bd4d9d0db90038f44db5c
kanishkambk43 <kanishkambk43@gmail.com>
2026-07-12 22:08:34 +0530

    .

--- binarysearch/firstbadversion.cpp
+++ binarysearch/firstbadversion.cpp
@@ -20,3 +20,23 @@ Example 2:
 Input: n = 1, bad = 1
 Output: 1*/
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=1;
+        int high =n;
+        int ans=1;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(isBadVersion(mid)){
+                ans=mid;
+                high=mid-1;
+                
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};

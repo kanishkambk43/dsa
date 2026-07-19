61f1fd49fd576e2b3a3bdd3139e86318febf1510
kanishkambk43 <kanishkambk43@gmail.com>
2026-07-19 21:18:54 +0530

    .

--- arrays/palindrome.cpp
+++ arrays/palindrome.cpp
@@ -18,3 +18,20 @@ Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  */
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s =to_string(x);
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
